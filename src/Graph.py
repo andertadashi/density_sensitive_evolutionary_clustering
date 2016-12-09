a20b56562b56ecbd38112a8165dc29a42b95f837
Tadashi Igarashi <tadashi.simepar@gmail.com>
2016-12-09 18:30:47 -0200

    Added graph_tool and GraphTest class
    Signed-off-by: Tadashi Igarashi <tadashi.simepar@gmail.com>

--- src/Graph.py
+++ src/Graph.py
@@ -1,9 +1,39 @@
-import networkx as nx
+import sys
+from graph_tool.all import *
 
 
-# class Graph:
-#
-#     def __init__(self):
-#         pass
-#
-#     def
+class GraphTest:
+
+    def __init__(self):
+        pass
+
+    def get_graph_dictionary(self, data, dissimilarity):
+        N = len(data)
+
+        print("graph V={}".format(N))
+
+        # g = Graph()
+        g = Graph(directed=False)
+        e_weight = g.new_edge_property("float")
+        list = []
+        dist = []
+        for i in range(N):
+            for j in range(N):
+                if i == j:
+                    continue
+                d = dissimilarity.distance(i, j)
+                # print dist
+                assert dist >= 0, "dist is negative!"
+                list.append((i, j))
+                dist.append(d)
+        print("add list to graph")
+        g.add_edge_list(list)
+        print("add weight to graph")
+        e_weight.a = dist
+
+        for i in range(N):
+            for j in range(N):
+                if i == j:
+                    continue
+                d = graph_tool.topology.shortest_distance(g, source=g.vertex(i), target=g.vertex(j), weights=e_weight)
+                print("i={} j={} dist={}".format(i, j, d))
--- src/test_plot.py
+++ src/test_plot.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python
 
+import sys
 from Algorithms import Cluster
 from Database import DatabaseSimepar
 from Dissimilarity import DensityDistance
 from Database import DatabaseIris
 from Database import TwoDimensionData
+from Graph import GraphTest
+# from graph_tool.all import *
 
+print("database reading")
+database = TwoDimensionData("../database/R15.txt", '\t')
 
-database = TwoDimensionData("../database/D31.txt", '\t')
+print("dissimilarity class")
 dissimilarity = DensityDistance(rho=1.2)
 
 data = database.data
 labels = database.labels
 
-cluster = Cluster(database, dissimilarity, P_size=5, K=3, max_iterations=10)
+# cluster = Cluster(database, dissimilarity, P_size=5, K=3, max_iterations=10)
+# cluster.compute()
+
+print("create graph")
+
+g = GraphTest()
+
+g.get_graph_dictionary(data, dissimilarity)
 
 
-cluster.compute()
