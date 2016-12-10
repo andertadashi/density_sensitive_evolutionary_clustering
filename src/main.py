53891b06f2e1b47cd1d9510e4e5c408ff9809f99
Tadashi Igarashi <tadashi.simepar@gmail.com>
2016-12-09 22:40:20 -0200

    computing distance for a range rho
    Signed-off-by: Tadashi Igarashi <tadashi.simepar@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import numpy as np
 from Algorithms import Cluster
 from Database import DatabaseSimepar
 from Dissimilarity import DensityDistance
@@ -7,8 +8,9 @@ from Database import DatabaseIris, TwoDimensionData
 
 # database = DatabaseIris()
 database = TwoDimensionData('../database/spiral.txt', '\t')
-dissimilarity = DensityDistance(rho=1.2)
+for rho in np.arange(1, 3, 0.2):
+    dissimilarity = DensityDistance(rho=rho)
 
-cluster = Cluster(database, dissimilarity, P_size=5, K=3, max_iterations=10)
+    cluster = Cluster(database, dissimilarity, P_size=5, K=3, max_iterations=10)
 
-cluster.compute()
+    # cluster.compute()
