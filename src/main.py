38f96aa13b10612c45be6280bf315ed49f69ed98
Tadashi Igarashi <tadashi.simepar@gmail.com>
2016-12-10 12:59:36 -0200

    Pickle files
    Signed-off-by: Tadashi Igarashi <tadashi.simepar@gmail.com>

--- src/main.py
+++ src/main.py
@@ -7,10 +7,16 @@ from Dissimilarity import DensityDistance
 from Database import DatabaseIris, TwoDimensionData
 
 # database = DatabaseIris()
-database = TwoDimensionData('../database/spiral.txt', '\t')
-for rho in np.arange(1, 3, 0.2):
-    dissimilarity = DensityDistance(rho=rho)
 
-    cluster = Cluster(database, dissimilarity, P_size=5, K=3, max_iterations=10)
+a = ['flame.txt' ,  'jain.txt' ]
+#a = ['Aggregation.txt'  ,'Compound.txt'  ,'D31.txt'  ,'flame.txt' ,  'jain.txt' , 'pathbased.txt' , 'R15.txt' , 'spiral.txt']
+for f in reversed(a):
+    print f
+    fi = '../database/{}'.format(f)
+    database = TwoDimensionData(fi, '\t')
+    for rho in np.arange(1.0, 3, 0.2):
+        dissimilarity = DensityDistance(rho=rho)
+
+        cluster = Cluster(database, dissimilarity, P_size=5, K=2, max_iterations=10)
 
     # cluster.compute()
