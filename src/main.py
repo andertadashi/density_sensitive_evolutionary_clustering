3eb0cd35704e2128cd6a30b6f56c66a8681f6b73
Tadashi Igarashi <tadashi.simepar@gmail.com>
2016-12-13 15:05:41 -0200

    new main to print avg results of executions
    Signed-off-by: Tadashi Igarashi <tadashi.simepar@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 import numpy as np
 import os
 from Algorithms import Cluster
@@ -11,30 +11,56 @@ from Database import DatabaseIris, TwoDimensionData
 # a = ['Compound.txt'  , 'flame.txt' ,'D31.txt',  'jain.txt' , 'pathbased.txt' , 'R15.txt' , 'spiral.txt']
 #a = ['Aggregation.txt' ] 
 # a = ['flame.txt', 'jain.txt']
-#a = [('spiral.txt', 3)]
+# a = [('spiral.txt', 3)]
 #a = [('R15.txt', 15)]
 #a = [('Compound.txt', 6)]
 
-a = [('flame.txt', 2), ('pathbased.txt', 3), ('spiral.txt', 3), ('jain.txt', 2), ('Compound.txt', 6), ('R15.txt', 15)]
+a = [('flame.txt', 2), ('pathbased.txt', 3), ('spiral.txt', 3), ('jain.txt', 2),
+     ('Compound.txt', 6), ('R15.txt', 15)]
 
-results = []
+results_lines = []
+results = {}
+for t in range(10):
+    file_dict = {}
+    for f, K in a:
+        fi = '../datasets/{}'.format(f)
+        database = TwoDimensionData(fi, '\t')
 
-for f, K in a:
-    fi = '../datasets/{}'.format(f)
-    database = TwoDimensionData(fi, '\t')
+        base_name = os.path.basename(fi)
+        name = os.path.splitext(base_name)[0]
+        rhos = {}
+        # for rho in np.arange(1.0, 3.4, 0.2):
+        # for rho in [1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 10, 30, 100]:
+        for rho in [1.0, 1.2, 1.4]:
 
-    base_name = os.path.basename(fi)
-    name = os.path.splitext(base_name)[0]
+            if (f, rho) in results:
+                result_vect = results[(f, rho)]
+            else:
+                result_vect = []
 
-    # for rho in np.arange(1.0, 3.4, 0.2):
-    for rho in [1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 10, 30, 100]:
-        dissimilarity = DensityDistance(rho=rho)
+            dissimilarity = DensityDistance(rho=rho)
 
-        cluster = Cluster(database, dissimilarity, P_size=50, K=K, max_iterations=50)
+            cluster = Cluster(database, dissimilarity, P_size=50, K=K, max_iterations=70)
 
-        score, score_normalized = cluster.compute()
-        results.append((name, K, rho, score, score_normalized))
+            score, score_normalized = cluster.compute()
+            results_lines.append("{};{};{};{};{};{}".format(t, f, K, rho, score, score_normalized))
+
+            result_vect.append((score, score_normalized))
+            results[(f, rho)] = result_vect
+
+print("\n\nresultados:")
+for rl in results_lines:
+    print rl
 
 
-for name, K, rho, score, score_normalized in results:
-    print("{};{};{};{:.8f};{:.8f}".format(name, K, rho, score, score_normalized))
+print("\n\nmédias:")
+for key in results.keys():
+
+    score_vector = results[key][0]
+    score_norm_vector = results[key][1]
+
+    score_vector_avg = np.sum(score_vector) / len(score_vector)
+    score_norm_vector_avg = np.sum(score_norm_vector) / len(score_norm_vector)
+
+    print ("file={} rho={} size={} score_avg={} score_norm_avg={}".format(
+        key[0], key[1], len(results), score_vector_avg, score_norm_vector_avg))
